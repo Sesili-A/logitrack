b2049fedcf0cd987735ed9bea17ec35deb036ef9
sesili <sesilianthonypeter@gmail.com>
2026-05-05 20:00:48 +0530

    notification

--- admin-frontend/public/sw.js
+++ admin-frontend/public/sw.js
@@ -68,20 +68,23 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener('push', function(event) {
   if (event.data) {
-    const data = event.data.json();
-    const options = {
-      body: data.body,
-      icon: data.icon || '/logo.png',
-      badge: '/logo.png',
-      vibrate: [100, 50, 100],
-      data: {
-        dateOfArrival: Date.now(),
-        primaryKey: '2'
-      }
-    };
-    event.waitUntil(
-      self.registration.showNotification(data.title, options)
-    );
+    try {
+      const data = event.data.json();
+      const options = {
+        body: data.body,
+        vibrate: [200, 100, 200, 100, 200, 100, 200],
+        requireInteraction: true,
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: '2'
+        }
+      };
+      event.waitUntil(
+        self.registration.showNotification(data.title || "New Alert", options)
+      );
+    } catch (err) {
+      console.error("Push Event Error:", err);
+    }
   }
 });
 
